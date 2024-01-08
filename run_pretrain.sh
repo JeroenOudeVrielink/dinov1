2ebfd6e55dd62624bc58213b7399a2f995bd2ee6
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-09 09:28:16 +1030

    chaning num workers

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -1,3 +1,4 @@
+# Num workers was 14 (optimal) but caused OOM errors for RAM on ws7
 
 python main_dino.py \
 --exp_name dinov1_base_params_bs128_ep400 \
@@ -11,7 +12,7 @@ python main_dino.py \
 --global_crops_scale 0.14 1 \
 --local_crops_scale 0.05 0.14 \
 --batch_size_per_gpu 128 \
---num_workers 14 \
+--num_workers 10 \
 --wandb_log_freq 100 \
 --saveckp_freq 10 \
 --epochs 400
