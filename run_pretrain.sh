d9f448c714adc6378e82ec8295b4b4c7c6f5b23e
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-19 16:42:08 +1030

    adding name

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -1,7 +1,7 @@
 # Num workers was 14 (optimal) but caused OOM errors for RAM on ws7
 
 python main_dino.py \
---exp_name test \
+--exp_name dinov1_bs164_global448_local192_ep400 \
 --output_dir /dinov1_models \
 --data_path /jvrielink/AIML_rot_corrected \
 --arch resnet50 \
@@ -16,6 +16,6 @@ python main_dino.py \
 --wandb_log_freq 100 \
 --saveckp_freq 10 \
 --epochs 400 \
---global_crop_size 512 \
---local_crop_size 224
+--global_crop_size 448 \
+--local_crop_size 192
 
