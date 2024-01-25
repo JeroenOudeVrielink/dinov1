3c2580810e2beed69760b927f26281f4aecf8f1f
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-01-25 14:53:34 +1030

    adding stuff

--- run_pretrain_no_local_to_global.sh
+++ run_pretrain_no_local_to_global.sh
@@ -3,7 +3,7 @@
 # Num workers was 14 (optimal) but caused OOM errors for RAM on ws7
 
 python main_dino.py \
---exp_name test_no_multi_crop_no_local_to_global \
+--exp_name test_no_local_to_global \
 --output_dir /home/jvrielink/data/dinov1_models \
 --data_path /home/jvrielink/AIML_rot_corrected \
 --arch resnet50 \
@@ -11,12 +11,12 @@ python main_dino.py \
 --lr 0.03 \
 --weight_decay 1e-4 \
 --weight_decay_end 1e-4 \
---global_crops_scale 0.6 1 \
+--global_crops_scale 0.9 1 \
+--local_crops_scale 0.9 1 \
 --batch_size_per_gpu 128 \
 --num_workers 10 \
 --wandb_log_freq 100 \
 --saveckp_freq 10 \
---epochs 301 \
---local_crops_number 0
+--epochs 301
 
 
