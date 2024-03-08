532c71b72454fc30e6261da0fcd76446d6c7491f
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-03-08 16:02:32 +1030

    adding new run files

--- run_test_augmentations.sh
+++ run_test_augmentations.sh
@@ -2,7 +2,7 @@
 
 # Num workers was 14 (optimal) but caused OOM errors for RAM on ws7
 
-./run_pretrain_random_rotations.sh
+./run_pretrain_random_rotation.sh
 
 ./run_pretrain_smoothing_instead_blurring.sh
 
