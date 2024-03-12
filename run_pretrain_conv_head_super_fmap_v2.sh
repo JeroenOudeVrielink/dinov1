77636251c1b05f891882b8351e331b22185f8185
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-03-12 16:46:26 +1030

    adding run file

--- run_pretrain_conv_head_super_fmap_v2.sh
+++ run_pretrain_conv_head_super_fmap_v2.sh
@@ -3,9 +3,10 @@
 # Num workers was 14 (optimal) but caused OOM errors for RAM on ws7
 
 python main_dino.py \
---exp_name dinov1_bs128_conv_head_v3 \
---output_dir ~/data/dinov1_models \
---data_path ~/AIML_rot_corrected \
+--from_ckpt True \
+--exp_name dinov1_bs64_conv_head_super_fmap \
+--output_dir /dinov1_bs64_conv_head_super_fmap_2024-03-08_05-10-07 \
+--data_path /AIML_rot_corrected \
 --arch resnet50 \
 --optimizer sgd \
 --lr 0.03 \
