6e79da4f95e574c4de6925e9da2753c4e809099a
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-03-13 15:13:04 +1030

    fixing small mistake

--- data_augmentation.py
+++ data_augmentation.py
@@ -217,8 +217,9 @@ class CustomDataAugmentationDINO(object):
         )
         normalize = transforms.Compose(
             [
+                transforms.Grayscale(num_output_channels=3),
                 transforms.ToTensor(),
-                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+                # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
             ]
         )
 
