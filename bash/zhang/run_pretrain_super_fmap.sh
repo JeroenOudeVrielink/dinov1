25777fd5b9314fd99cf816c7c5fca4b2fd0e6bd3
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-03-15 12:41:18 +1030

    adding new run script

--- bash/zhang/run_pretrain_super_fmap.sh
+++ bash/zhang/run_pretrain_super_fmap.sh
@@ -2,10 +2,12 @@
 
 # Num workers was 14 (optimal) but caused OOM errors for RAM on ws7
 
+cd ../..
+
 python main_dino.py \
---exp_name dinov1_bs64_conv_head_v3 \
---output_dir ~/data/dinov1_models \
---data_path ~/AIML_rot_corrected \
+--exp_name dinov1_bs128_super_fmap_v3 \
+--output_dir /dinov1_models \
+--data_path /AIML_rot_corrected \
 --arch resnet50 \
 --optimizer sgd \
 --lr 0.03 \
@@ -13,10 +15,10 @@ python main_dino.py \
 --weight_decay_end 1e-4 \
 --global_crops_scale 0.14 1 \
 --local_crops_scale 0.05 0.14 \
---batch_size_per_gpu 64 \
+--batch_size_per_gpu 128 \
 --num_workers 10 \
 --wandb_log_freq 100 \
 --saveckp_freq 20 \
---epochs 61 \
+--epochs 401 \
 --use_conv_head True \
 --out_dim 50176
