c6e659e312bc20656c3ff2c5b83c84c089d67a06
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-03-18 12:11:20 +1030

    adjusting run script

--- bash/run_pretrain_param_tune.sh
+++ bash/run_pretrain_param_tune.sh
@@ -3,7 +3,7 @@
 # Num workers was 14 (optimal) but caused OOM errors for RAM on ws7
 
 python main_dino.py \
---exp_name dinov1_bs128_no_norm_last_layer \
+--exp_name dinov1_bs128_increased_crop_scale_v2 \
 --output_dir /dinov1_models \
 --data_path /AIML_rot_corrected \
 --arch resnet50 \
@@ -11,45 +11,8 @@ python main_dino.py \
 --lr 0.03 \
 --weight_decay 1e-4 \
 --weight_decay_end 1e-4 \
---global_crops_scale 0.14 1 \
---local_crops_scale 0.05 0.14 \
---batch_size_per_gpu 128 \
---num_workers 14 \
---wandb_log_freq 100 \
---saveckp_freq 20 \
---epochs 61 \
---norm_last_layer False
-
-
-python main_dino.py \
---exp_name dinov1_bs128_0.9995momentum_teacher \
---output_dir /dinov1_models \
---data_path /AIML_rot_corrected \
---arch resnet50 \
---optimizer sgd \
---lr 0.03 \
---weight_decay 1e-4 \
---weight_decay_end 1e-4 \
---global_crops_scale 0.14 1 \
---local_crops_scale 0.05 0.14 \
---batch_size_per_gpu 128 \
---num_workers 14 \
---wandb_log_freq 100 \
---saveckp_freq 20 \
---epochs 61 \
---momentum_teacher 0.9995
-
-python main_dino.py \
---exp_name dinov1_bs128_increased_crop_scale \
---output_dir /dinov1_models \
---data_path /AIML_rot_corrected \
---arch resnet50 \
---optimizer sgd \
---lr 0.03 \
---weight_decay 1e-4 \
---weight_decay_end 1e-4 \
---global_crops_scale 0.5 1 \
---local_crops_scale 0.25 0.5 \
+--global_crops_scale 0.32 1 \
+--local_crops_scale 0.1 0.32 \
 --batch_size_per_gpu 128 \
 --num_workers 14 \
 --wandb_log_freq 100 \
