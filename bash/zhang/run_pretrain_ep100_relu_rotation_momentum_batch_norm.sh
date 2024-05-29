ff39025c3cd7da4168743c5da5b8ff4e1a4ab2ee
JeroenOudeVrielink <jeroenov98@gmail.com>
2024-05-29 17:18:13 +0930

    add stuff

---
+++ bash/zhang/run_pretrain_ep100_relu_rotation_momentum_batch_norm.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Num workers was 14 (optimal) but caused OOM errors for RAM on ws7
+
+cd ../..
+
+python main_dino.py \
+--exp_name dinov1_bs128_ep100_relu_rotation_momentum_batch_norm \
+--output_dir /dinov1_models \
+--data_path /AIML_rot_corrected \
+--arch resnet50 \
+--optimizer sgd \
+--lr 0.03 \
+--weight_decay 1e-4 \
+--weight_decay_end 1e-4 \
+--global_crops_scale 0.14 1 \
+--local_crops_scale 0.05 0.14 \
+--batch_size_per_gpu 128 \
+--num_workers 14 \
+--wandb_log_freq 100 \
+--saveckp_freq 20 \
+--epochs 101 \
+--activation "relu" \
+--custom_augmentation True \
+--p_random_rotation 0.5 \
+--momentum_teacher 0.9995 \
+--use_bn_in_head True
